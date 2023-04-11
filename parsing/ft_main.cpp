55df6a4dc3ada7b12cf1b60a32bbf88d1586c018
mobo-9-0045 <abdelilahoman@gmail.com>
2023-04-11 15:23:12 +0000

    OK

--- parsing/ft_main.cpp
+++ parsing/ft_main.cpp
@@ -22,15 +22,11 @@ int parsing(int argc, char **argv)
 		(void)argc;
 		Config c(argv);
 		std::cout << c.s[1].L[0].error_cods[0] << std::endl;
-		std::cout << c.s[0].L[0].index_val << std::endl;
 		std::cout << c.s[0].L[0].autoindex << std::endl;
+		std::cout << c.s[1].L[0].redirec << std::endl;
 		std::cout << c.s[0].L[1].root_val << std::endl;
-		std::cout << c.s[0].L[1].autoindex << std::endl;
-		std::cout << c.s[0].L[1].index_val << std::endl;
-		std::cout << c.s[0].L[2].index_val << std::endl;
-		std::cout << c.s[0].L[2].upload << std::endl;
-		std::cout << c.s[0].port << std::endl;
-		cgi(c.s[0]);
+		std::cout << c.s[0].L[2].root_val << std::endl;
+		// cgi(c.s[0]);
 		return (0);
 	}
 	return (1);
