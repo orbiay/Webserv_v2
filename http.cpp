4879a607d9b95838d0932dbe08f49bd9bfb6d45c
Oussama Rbiay <orbiay@e1r9p5.1337.ma>
2023-04-17 20:44:49 +0000

    bind the posrt and host name

--- http.cpp
+++ http.cpp
@@ -32,10 +32,20 @@ void Http::naming_socket(struct sockaddr_in address )
     def_socket = address;
 }
 
-void Http::bind()
+void Http::bind(Pserver &conf)
 {
     int use = 1;
+    struct addrinfo hints;
+    hints.ai_family = AF_INET; 
+    hints.ai_socktype = SOCK_STREAM; 
+    struct addrinfo* results;
     setsockopt(fd_server,SOL_SOCKET,SO_REUSEADDR, &use,sizeof(use));
+    std::cout<<conf.host.c_str()<<std::endl;
+    int status = getaddrinfo(conf.host.c_str(), "http", &hints, &results);
+    if (status != 0) {
+        std::cerr << "Failed to perform DNS lookup: " << gai_strerror(status) << std::endl;
+         exit(2); ;
+    }
     if (::bind(fd_server,(struct sockaddr *)&def_socket,sizeof(def_socket)) < 0)
     {
         std::cerr<<"can't bind socket with def_socket."<<std::endl;
